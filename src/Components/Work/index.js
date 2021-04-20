6bf996bbe59b0a38e03c20a177e4ffdbf47a9314
Hazem Khalil <hazem-khalil@hotmail.com>
2021-04-20 23:33:38 +0200

    Updating

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { WorkSection, WorkTitle, Span, Page, Hr } from "./style.js";
 
 
-const Work =()=> {
-    
-        return (
-            <WorkSection id="work">
-                <div className="container">
-                    <WorkTitle><Span>My</Span> Work</WorkTitle>
-                    <Hr/>
-                    <Page src="https://shhm8905.github.io/rock-game/"></Page>
-                    <Page src="https://shhm8905.github.io/Transfer-App/"></Page>
-                    <Page src="https://shhm8905.github.io/Tamplate-1-css-html/index.html"></Page>
-                </div>
-            </WorkSection>
-        );
-    
+const Work = () => {
+
+    return (
+        <WorkSection id="work">
+            <div className="container">
+                <WorkTitle><Span>My</Span> Work</WorkTitle>
+                <Hr />
+                <Page src="https://shhm8905.github.io/rock-game/"></Page>
+                <Page src="https://shhm8905.github.io/Transfer-App/"></Page>
+            </div>
+        </WorkSection>
+    );
+
 }
 
-export default Work; 
+export default Work;
